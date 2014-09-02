44631c98bf520c92366b6b1cc46e47aec70fe145
Frederick <fdong2@gmail.com>
2014-09-02 18:22:25 -0400

    version 1 of BASH script

---
+++ buggy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Day 1 - Homework: Part 2 - debug this bash script
+#
+
+echo "There are around 6 mistakes"
+
+FASTQ_DIR=/Users/cmdb/data/fastq
+OUTPUT_DIR=/Users/cmdb/data/day1
+SAMPLE_PREFIX=SRR072
+
+GENOME_DIR=/Users/cmdb/data/genomes
+GENOME_PREFIX=dmel5
+ANNOTATION=dmel-all-r5.57.gff
+
+CORES=4
+
+for i in {1..24}
+do
+  echo gunzip $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz	
+  echo fastqc $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq -o $OUTPUT_DIR
+  echo tophat -p$CORES -G $OUTPUT_DIR/ANNOTATION -o $OUTPUT_DIR $GENOME_DIR/$GENOME_PREFIX $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq
+  echo cufflinks -p$CORES -G $OUTPUT_DIR/ANNOTATION -o #need .sam file?
+done
